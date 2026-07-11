a892b2a5cb38946d03c9bbc02eeab4a520657209
Erik Contreras <contreras.eee@gmail.com>
2026-07-10 22:44:10 -0400

    test: implementar pruebas unitarias para taskinput

--- src/test/setup.js
+++ src/test/setup.js
@@ -1,3 +1,12 @@
 // src/test/setup.js
-import '@testing-library/jest-dom'
+import { expect, beforeEach } from 'vitest'
+import * as matchers from '@testing-library/jest-dom/matchers'
+import { cleanup } from '@testing-library/react'
 
+// Le inyectamos los métodos de validación a Vitest
+expect.extend(matchers)
+
+//AGREGA ESTO: Limpia el DOM simulado automáticamente antes de cada prueba
+beforeEach(() => {
+  cleanup()
+})
